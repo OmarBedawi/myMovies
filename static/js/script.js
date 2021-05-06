1b9aebc30fee63c3f8e13bd609d511b3951eb390
OmarBedawi <omarbedawi@hotmail.com>
2021-05-06 20:44:31 +0000

    adjusted styling around the pages. all files validated and formatted

--- static/js/script.js
+++ static/js/script.js
@@ -2,24 +2,11 @@
     jQuery for MaterializeCSS initialization
 */
 
-$(document).ready(function () {
-    $(".sidenav").sidenav({edge: "right"});
-    $(".collapsible").collapsible();
-    $(".datepicker").datepicker({
-        format: "yyyy",
-        yearRange: 100,
-        showClearBtn: true,
-        i18n: {
-            done: "Select"
-        }
-    });
-});
-
 
 $('.comment_submit').on('click', function(event){
     filmId = $(this).attr('data-film-id');
     comment = $('#' + $(this).attr('data-target_id')).val();
-    requestData = {'id': filmId, 'comment': comment}
+    requestData = {'id': filmId, 'comment': comment};
     console.log('add comment to Film id', filmId, comment);
     $.post( "/comment", requestData, function( data ) {
         commentsTargetId = '#film_comment_' + filmId;
